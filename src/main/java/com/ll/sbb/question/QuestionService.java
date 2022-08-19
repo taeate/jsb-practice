6e3060fdf2b541a8dc849a2f902376930bbb2698
taeate <yes3144@nate.com>
2022-08-19 14:33:05 +0900

    QuesionService 도입

---
+++ src/main/java/com/ll/sbb/question/QuestionService.java
@@ -0,0 +1,18 @@
+package com.ll.sbb.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+  
+  private final QuestionRepository questionRepository;
+
+  public List<Question> getList(){
+    return this.questionRepository.findAll();
+  }
+}
