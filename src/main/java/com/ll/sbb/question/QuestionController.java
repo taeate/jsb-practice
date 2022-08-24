765ff91a5be35470b8125bb422d759b28ac916cd
taeate <yes3144@nate.com>
2022-08-24 17:56:35 +0900

    create form 구현

--- src/main/java/com/ll/sbb/question/QuestionController.java
+++ src/main/java/com/ll/sbb/question/QuestionController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,5 +34,14 @@ public class QuestionController {
 
     return "question_detail";
   }
-
+  @GetMapping("/create")
+  public String create(){
+    return "question_form";
+  }
+  @PostMapping("/create")
+  public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+    // TODO 질문을 저장한다.
+    this.questionService.create(subject, content);
+    return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+}
 }
