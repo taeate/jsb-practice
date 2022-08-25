817d335ab126062d56aea45e8f202cec345449f3
taeate <yes3144@nate.com>
2022-08-25 15:51:48 +0900

    User repository, service 생성

---
+++ src/main/java/com/ll/sbb/user/UserService.java
@@ -0,0 +1,24 @@
+package com.ll.sbb.user;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public SiteUser create(String username, String password, String email){
+
+    SiteUser user = new SiteUser();
+    user.setUsername(username);
+    user.setEmail(email);
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    user.setPassword(passwordEncoder.encode(password));
+    this.userRepository.save(user);
+    return user;
+  }
+}
